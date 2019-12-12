6bbf4a63a5beb8db7b138864f43c8cd6448a33e6
zcb0015 <zcb0015@unt.edu>
2019-12-12 01:07:59 -0600

    knapsack using earn and weight

--- readfile.cpp
+++ readfile.cpp
@@ -3,14 +3,16 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
+// class to hold item properties
 class item {
-	public:
+public:
 	string name;
 	int weight;
-	int cost;
+	double cost;
 	string address;
 	double value;
 	double earn;
@@ -23,8 +25,22 @@ class item {
 		value = ((double)c)/((double)w);
 		earn = ((double)c)/10;
 	}
+	
+	// define operator to help sort items	
+	bool operator> (const item &other) const {
+		return value > other.value;
+	}
 };
 
+
+// sort items by their value (cost/weight)
+vector<item> itemSorter(vector<item> I) {
+	vector<item> items_temp = I;
+	sort(items_temp.begin(), items_temp.end(), greater <>());
+	return items_temp;
+}
+
+//prints all Items
 void printItems(vector<item> I)
 {
 	cout << "Name\t\t\tWeight\tCost\tValue\tEarn\tAddress\n";
@@ -38,24 +54,56 @@ void printItems(vector<item> I)
 	}
 }
 
+// utility function returns max of 2 ints
+double max(double a, double b) { return (a > b)? a : b; }
+
+// Returns the max value to put into truck
+double knapSack(int W, vector<item> I, int n)
+{
+	int i, w;
+	double K[n+1][W+1];
+
+	//Build table K[][] in bottom up manner
+	for (i=0;i<=n;i++) 
+	{
+		for (w=0;w<=W;w++)
+		{
+			if(i==0 || w==0)
+				K[i][w] =0.0;
+			else if(I[i-1].weight <= w)
+				K[i][w] = (max(I[i-1].earn + (double)K[i-1][w - I[i-1].weight], K[i-1][w]));
+			else
+				K[i][w] = (K[i-1][w]);
+		}
+	}
+
+	return (K[n][W]);
+}
+
 int main()
 {
-	//store items
-	vector<item>Items;
+	// store items
+	vector<item> Items;
 
 	string filename;
-	//ask user for filename of item set
+	// ask user for filename of item set
 	cout << "Which file of item input to test?" << endl;
 	getline(cin, filename);
-	
-	//read file for items
+
+	int W = 100;	
+	// ask user for maximum weight W
+	cout << "What is the max weight, W, that the truck can carry?" << endl;
+       	cin >> W;	
+
+
+	// read file for items
 	ifstream file(filename.c_str());
 	while(file)
 	{
 		string s;
 		if(!getline(file,s)) break;
 		
-		//split row into values seperated by commas
+		// split row into values seperated by commas
 		istringstream ss(s);
 		vector<string> row;
 		row.clear();
@@ -65,15 +113,20 @@ int main()
 			if(!getline(ss, value, ',')) break;
 			row.push_back(value);
 		}
-		//create temp item from values to add to vector
-		item temp(row[0],stoi(row[1]),stoi(row[2]),row[3]);
+		// create temp item from values to add to vector
+		item temp(row[0],stoi(row[1]),stod(row[2]),row[3]);
 		Items.push_back(temp);
 	}
 
 	file.close();
-	
 
-	printItems(Items);
+	
+	Items = itemSorter(Items);
+
+	int n = Items.size();
+	cout << knapSack(W,Items,n) << endl;
+
+//	printItems(Items);
 
 	return 0;
 }
